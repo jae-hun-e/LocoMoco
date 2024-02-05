e08a37a17f6916739724c29c0addedbd185e8469
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-06 02:20:54 +0900

    chore: commitLint setting

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'type-case': [2, 'always', 'lowerCase'], // type 소문자로만
+    'type-empty': [2, 'never'], // type 생략 불가
+    'type-enum': [
+      // 아래의 type 만 사용 가능
+      2,
+      'always',
+      ['init', 'feat', 'fix', 'refactor', 'style', 'chore', 'remove'],
+    ],
+    'subject-empty': [2, 'never'], // subject 생략 불가
+  },
+};
