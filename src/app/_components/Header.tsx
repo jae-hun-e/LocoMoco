9f691b59493a929ef0746f7ea98768b4fdfd7077
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-07 00:38:47 +0900

    feat: NavBar 라우팅 처리 + 아이콘 추가

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,9 +1,11 @@
+import { AlignJustifyIcon, ChevronLeftIcon } from 'lucide-react';
+
 const Header = () => {
   return (
     <div className="flex h-14 w-full items-center justify-around">
-      <div>1</div>
+      <ChevronLeftIcon />
       <div>LocoMoco</div>
-      <div>...</div>
+      <AlignJustifyIcon />
     </div>
   );
 };
