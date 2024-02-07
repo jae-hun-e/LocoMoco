cf521c6f50f195e9ddfa7af84544fe91227e0d7e
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-07 19:26:41 +0900

    feat: axios core
    ps. 갓재민 감사히 쓰겠습니다!(꾸벅)

--- src/apis/core.ts
+++ src/apis/core.ts
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
 
 // todo: bsaeURL은 추후 환경변수로 빼야함 [24/02/07]
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
-const axiosInstance = axios.create({
+
+const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
@@ -22,3 +23,30 @@ axiosInstance.interceptors.response.use(
   (res) => res.data,
   (error) => Promise.reject(error),
 );
+
+const createApiMethod =
+  (_axiosInstance: AxiosInstance, methodType: Method) =>
+  <T>(config: AxiosRequestConfig): Promise<T> => {
+    return _axiosInstance({
+      ...config,
+      method: methodType,
+    });
+  };
+
+const HTTP_METHODS = {
+  GET: 'get',
+  POST: 'post',
+  PATCH: 'patch',
+  PUT: 'put',
+  DELETE: 'delete',
+} as const;
+
+const client = {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
+
+export default client;
