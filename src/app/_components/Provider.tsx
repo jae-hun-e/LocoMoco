3ff98854e89f85cd19569db6d4a6d25fd4f53fcd
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-07 15:42:53 +0900

    feat: tanstack query provider 설정

---
+++ src/app/_components/Provider.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const Provider = ({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+            retry: false,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default Provider;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import Navbar from '@/app/_components/NavBar';
+import Provider from '@/app/_components/Provider';
 import { cn } from '@/libs/utils';
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
@@ -19,10 +20,12 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className={cn(inter.className, 'relative min-h-svh')}>
-        <main className={`min-h-[calc(100svh-3.125rem)]`}>{children}</main>
-        <Navbar />
-      </body>
+      <Provider>
+        <body className={cn(inter.className, 'relative min-h-svh')}>
+          <main className={`min-h-[calc(100svh-3.125rem)]`}>{children}</main>
+          <Navbar />
+        </body>
+      </Provider>
     </html>
   );
 };
