d0682819ce25d63866d3f0fce078f6c7c3eeaaee
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-07 23:59:38 +0900

    modify: 불필요한 useState 제거

--- src/app/_components/Provider.tsx
+++ src/app/_components/Provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -9,18 +9,15 @@ const Provider = ({
 }: Readonly<{
   children: ReactNode;
 }>) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-            refetchOnWindowFocus: false,
-            retry: false,
-          },
-        },
-      }),
-  );
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+        refetchOnWindowFocus: false,
+        retry: false,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
