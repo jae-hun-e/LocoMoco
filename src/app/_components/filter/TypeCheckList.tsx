7e5d62fab2320591446338b16bb5917e4b8cce2d
kimyou1102 <yoosk5485@naver.com>
2024-02-14 00:25:49 +0900

    modify: 인라인으로 작성된 핸들러 함수 분리

--- src/app/_components/filter/TypeCheckList.tsx
+++ src/app/_components/filter/TypeCheckList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Control } from 'react-hook-form';
+import { Control, ControllerRenderProps } from 'react-hook-form';
 import { Checkbox } from '@/components/ui/checkbox';
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { SearchFilterForm } from '@/types/searchFilterForm';
@@ -11,6 +11,26 @@ interface TypeCheckListProps {
 }
 
 const TypeCheckList = ({ types, control, type }: TypeCheckListProps) => {
+  const handleCheckedChange = (
+    checked: boolean | 'indeterminate',
+    item: string,
+    field: ControllerRenderProps<SearchFilterForm, keyof SearchFilterForm>,
+  ) => {
+    if (item === '전체') {
+      return checked ? field.onChange([...types]) : field.onChange([]);
+    } else {
+      if (field.value.includes('전체')) {
+        return checked
+          ? field.onChange([...field.value])
+          : field.onChange(field.value?.filter((value) => value !== item && value !== '전체'));
+      } else {
+        return checked
+          ? field.onChange([...field.value, item])
+          : field.onChange(field.value?.filter((value) => value !== item));
+      }
+    }
+  };
+
   return (
     <div className="overflow-y-auto">
       <FormField
@@ -32,25 +52,7 @@ const TypeCheckList = ({ types, control, type }: TypeCheckListProps) => {
                       <FormControl>
                         <Checkbox
                           checked={field.value?.includes(item)}
-                          onCheckedChange={(checked) => {
-                            if (item === '전체') {
-                              return checked ? field.onChange([...types]) : field.onChange([]);
-                            } else {
-                              if (field.value.includes('전체')) {
-                                return checked
-                                  ? field.onChange([...field.value])
-                                  : field.onChange(
-                                      field.value?.filter(
-                                        (value) => value !== item && value !== '전체',
-                                      ),
-                                    );
-                              } else {
-                                return checked
-                                  ? field.onChange([...field.value, item])
-                                  : field.onChange(field.value?.filter((value) => value !== item));
-                              }
-                            }
-                          }}
+                          onCheckedChange={(checked) => handleCheckedChange(checked, item, field)}
                         />
                       </FormControl>
                       <FormLabel className="text-sm font-normal">{item}</FormLabel>
