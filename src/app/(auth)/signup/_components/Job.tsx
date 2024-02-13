0521a883b047bac5165ffc35a6eb019878135f36
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-02-13 11:49:09 +0900

    chore: add _ to components

---
+++ src/app/(auth)/signup/_components/Job.tsx
@@ -0,0 +1,35 @@
+import Radio from '@/components/Radio';
+import RadioGroup from '@/components/RadioGroup';
+
+interface Props {
+  job: string;
+  setJob: (job: string) => void;
+}
+const Job = ({ job, setJob }: Props) => {
+  return (
+    <RadioGroup label="직업">
+      <Radio
+        selected={job}
+        setSelected={setJob}
+        value="worker"
+      >
+        현직자
+      </Radio>
+      <Radio
+        selected={job}
+        setSelected={setJob}
+        value="ready"
+      >
+        취준생
+      </Radio>
+      <Radio
+        selected={job}
+        setSelected={setJob}
+        value="etc"
+      >
+        기타
+      </Radio>
+    </RadioGroup>
+  );
+};
+export default Job;
