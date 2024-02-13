3c2c6a9bb2902226921fb4159153e9a481f33da2
kimyou1102 <yoosk5485@naver.com>
2024-02-13 23:38:58 +0900

    bug: 바텀시트 오픈시 생성버튼 클릭 안되는 문제 해결

--- src/app/(home)/_components/BottomSheet.tsx
+++ src/app/(home)/_components/BottomSheet.tsx
@@ -12,16 +12,14 @@ const BottomSheet = ({ children }: { children: ReactElement }) => {
         </div>
       </SheetTrigger>
       <SheetContent
+        id="sheet-content"
         side="bottom"
         className="main-bottom-sheet z-10 flex h-[70svh] flex-col gap-0 overflow-y-auto rounded-t-xl p-0 outline-none"
       >
         <div className="z-[100px] w-[100px] w-full py-3">
           <div className="mx-auto h-1 w-40pxr rounded-full bg-layer-4" />
         </div>
-        <div className="overflow-y-auto">
-          {children}
-          <div className="h-50pxr" />
-        </div>
+        <div className="overflow-y-auto">{children}</div>
       </SheetContent>
     </Sheet>
   );
