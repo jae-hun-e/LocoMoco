b87e4914abb875db3d7ba22f9a5765e89b5871bc
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-15 15:45:18 +0900

    test: env안쓴다면?

--- src/app/(home)/_components/Map.tsx
+++ src/app/(home)/_components/Map.tsx
@@ -2,27 +2,20 @@
 
 import { useEffect, useRef } from 'react';
 
-// declare global {
-//   interface Window {
-//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//     kakao: any;
-//   }
-// }
-
 const Map = () => {
   const mapRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    window.kakao.maps.load(function () {
-      if (mapRef.current != null) {
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3,
-        };
-        new window.kakao.maps.Map(mapRef.current, mapOption);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.kakao.maps.load(function () {
+  //     if (mapRef.current != null) {
+  //       const mapOption = {
+  //         center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+  //         level: 3,
+  //       };
+  //       new window.kakao.maps.Map(mapRef.current, mapOption);
+  //     }
+  //   });
+  // }, []);
 
   useEffect(() => {
     const kakaoMapScript = document.createElement('script');
