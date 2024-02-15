b8af9979caf5ccefc2f3289917632b34664b81dd
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-15 14:59:09 +0900

    test: 카카오 맵 배포환경 테스트

--- src/app/(home)/_components/Map.tsx
+++ src/app/(home)/_components/Map.tsx
@@ -5,16 +5,36 @@ import { useEffect, useRef } from 'react';
 const Map = () => {
   const mapRef = useRef<HTMLDivElement>(null);
 
+  // useEffect(() => {
+  //   window.kakao.maps.load(function () {
+  //     if (mapRef.current != null) {
+  //       const mapOption = {
+  //         center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+  //         level: 3,
+  //       };
+  //       new window.kakao.maps.Map(mapRef.current, mapOption);
+  //     }
+  //   });
+  // }, []);
+
   useEffect(() => {
-    window.kakao.maps.load(function () {
-      if (mapRef.current != null) {
-        const mapOption = {
+    const kakaoMapScript = document.createElement('script');
+    kakaoMapScript.async = false;
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
+    document.head.appendChild(kakaoMapScript);
+
+    const onLoadKakaoAPI = () => {
+      window.kakao.maps.load(() => {
+        const options = {
           center: new window.kakao.maps.LatLng(33.450701, 126.570667),
           level: 3,
         };
-        new window.kakao.maps.Map(mapRef.current, mapOption);
-      }
-    });
+
+        new window.kakao.maps.Map(mapRef.current, options);
+      });
+    };
+
+    kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
   }, []);
 
   return (
