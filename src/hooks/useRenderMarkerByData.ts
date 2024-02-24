4d403688648bf0dc792aeb44db04b2ad26ac1efe
kimyou1102 <yoosk5485@naver.com>
2024-02-24 20:16:09 +0900

    fix: 콘솔로그 제거

--- src/hooks/useRenderMarkerByData.ts
+++ src/hooks/useRenderMarkerByData.ts
@@ -9,7 +9,6 @@ interface MakerInfo {
 const useRenderMarkerByData = (openSheetUpdate: (mapData: MGCSummary[]) => void) => {
   const setMarker = useCallback(
     (mapMGCData: MGCSummary[], clusterer: kakao.maps.MarkerClusterer) => {
-      console.log('setNarker', mapMGCData);
       const markersInfo = [] as MakerInfo[];
       // TODO: 임시 아이콘 추후에 변경해야함 [24.02.14]
       const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
@@ -27,7 +26,6 @@ const useRenderMarkerByData = (openSheetUpdate: (mapData: MGCSummary[]) => void)
         });
 
         kakao.maps.event.addListener(marker, 'click', () => {
-          console.log(marker, mgc);
           openSheetUpdate([mgc]);
         });
 
@@ -43,33 +41,19 @@ const useRenderMarkerByData = (openSheetUpdate: (mapData: MGCSummary[]) => void)
 
   const renderMarker = (clusterer: kakao.maps.MarkerClusterer, mapMGCData: MGCSummary[]) => {
     const markersInfo = setMarker(mapMGCData, clusterer);
-    console.log('markersInfo', markersInfo);
     clusterer.addMarkers(markersInfo.map((markerInfo) => markerInfo.marker));
 
     kakao.maps.event.addListener(clusterer, 'clusterclick', (cluster: kakao.maps.Cluster) => {
       const clusterMarkers = cluster.getMarkers();
       const markerList = [];
-      console.log('ffff', clusterMarkers);
-      console.log('eee', markersInfo);
-
-      // data: {모각코리스트들}, marker: {}
-      //   console.log('111', markersInfo[0].marker, clusterMarkers[0]);
-      //   console.log('000', markersInfo[0].marker === clusterMarkers[0]);
 
       for (const clusterMarker of clusterMarkers) {
-        for (const item of markersInfo) {
-          console.log('@@@ : ', item, clusterMarker);
-        }
-        //   console.log(
-        //     'ddd: ',
-        //     markersInfo.filter((markerInfo) => markerInfo.marker === clusterMarker),
-        //   );
         markerList.push(
           markersInfo.filter((markerInfo) => markerInfo.marker === clusterMarker)[0].data,
         );
       }
 
-      //   openSheetUpdate(markerList);
+      openSheetUpdate(markerList);
     });
   };
 
