c9ed6453f92cd6a37e5b77bb28fe7cca5f5b846b
kimyou1102 <yoosk5485@naver.com>
2024-02-24 18:00:20 +0900

    fix: 배포 오류 수정(더미데이터 타입 변경)

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,56 +1,54 @@
 'use client';
 
 import MGCList from '@/app/_components/MGCList/MGCList';
-import { MGCSummary } from '@/app/_components/MGCList/MGCListItem';
-import { LanguageTypes, MGCTypes, StudyTypes } from '@/constants/types';
+import { MGCSummary } from '@/types/MGCList';
 import { Search } from 'lucide-react';
 
 const dummyData: MGCSummary[] = [
   {
-    _id: 1,
+    id: 1,
     title: '모각코 구함',
-    location: '구로동',
-    createAt: new Date('1995-12-17T03:24:00'),
-    hits: 3,
+    location: {
+      address: '구로동',
+      latitude: 37.48,
+      longitude: 126.8,
+    },
+    // createAt: new Date('1995-12-17T03:24:00'),
+    views: 3,
     likeCount: 30,
-    currentParticipantsCount: 2,
-    maxParticipantsCount: 8,
-    MGCType: MGCTypes.LocationNotConfirmed,
-    languageTypes: [LanguageTypes.JAVA],
-    studyTypes: [StudyTypes.BE],
+    curParticipants: 2,
+    maxParticipants: 8,
+    tags: [1, 2, 3],
   },
   {
-    _id: 2,
+    id: 2,
     title: '면접 준비 같이해요~',
-    location: '남가좌동',
-    createAt: new Date('2023-12-17T03:24:00'),
-    hits: 3,
+    location: {
+      address: '구로동',
+      latitude: 37.48,
+      longitude: 126.8,
+    },
+    // createAt: new Date('2023-12-17T03:24:00'),
+    views: 3,
     likeCount: 30,
-    currentParticipantsCount: 2,
-    maxParticipantsCount: 3,
-    MGCType: MGCTypes.LocationConfirmed,
-    languageTypes: [LanguageTypes.JAVA, LanguageTypes.JAVASCRIPT],
-    studyTypes: [StudyTypes.BE, StudyTypes.FE, StudyTypes.web],
+    curParticipants: 2,
+    maxParticipants: 3,
+    tags: [1, 2, 3],
   },
   {
-    _id: 3,
+    id: 3,
     title: '번개 모각코 하실 분~!',
-    location: '구로동',
-    createAt: new Date('2024-02-14T00:24:00'),
-    hits: 10,
+    location: {
+      address: '구로동',
+      latitude: 37.48,
+      longitude: 126.8,
+    },
+    // createAt: new Date('2024-02-14T00:24:00'),
+    views: 10,
     likeCount: 20,
-    currentParticipantsCount: 1,
-    maxParticipantsCount: 5,
-    MGCType: MGCTypes.ThunderMGC,
-    languageTypes: [LanguageTypes.JAVA, LanguageTypes.JAVASCRIPT, LanguageTypes.PYTHON],
-    studyTypes: [
-      StudyTypes.BE,
-      StudyTypes.FE,
-      StudyTypes.web,
-      StudyTypes.BE,
-      StudyTypes.FE,
-      StudyTypes.web,
-    ],
+    curParticipants: 1,
+    maxParticipants: 5,
+    tags: [1, 2, 3],
   },
 ];
 
