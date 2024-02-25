61ee9d703ff2fd9803adef05d069f766ac4705a0
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-26 01:13:40 +0900

    modify: 쓸모없는 태그 삭제

--- src/app/create/loading.tsx
+++ src/app/create/loading.tsx
@@ -3,15 +3,13 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 const MGCCreateLoading = () => {
   return (
-    <>
-      <div className="flex flex-col space-y-3">
-        <Skeleton className="h-[125px] w-full rounded-xl" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-2/3" />
-        </div>
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-full rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-2/3" />
       </div>
-    </>
+    </div>
   );
 };
 
