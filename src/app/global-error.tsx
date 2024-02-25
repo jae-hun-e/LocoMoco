b0be6074d9cb95b7c9ce88436d8a7344a9e4cfe6
hun2__2 <wognskec@hanyang.ac.kr>
2024-02-26 01:15:32 +0900

    modify: global-error.tsx 문구 변경

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -10,7 +10,7 @@ const GlobalError = ({ error, reset }: ErrorFallBackProps) => {
   return (
     <html>
       <body>
-        <h2>렌더링 오류 :( </h2>
+        <h2>어플리케이션에 어떠한 오류가 발생했습니다. :( </h2>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
