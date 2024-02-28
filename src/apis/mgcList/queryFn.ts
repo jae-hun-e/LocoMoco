0bd04220c754f17cf16b8a01d8b971343d395bbf
kimyou1102 <yoosk5485@naver.com>
2024-02-28 17:45:54 +0900

    modify: axiosInstance에 paramsSerializer추가

--- src/apis/mgcList/queryFn.ts
+++ src/apis/mgcList/queryFn.ts
@@ -1,14 +1,10 @@
 import { MGCList } from '@/types/MGCList';
-import qs from 'qs';
 import client from '../core';
 
 export const getMGCList = async (tags?: number[]) => {
   const { data } = await client.get<MGCList>({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/mogakko/map`,
     params: { tags },
-    paramsSerializer: (params) => {
-      return qs.stringify(params, { arrayFormat: 'repeat' });
-    },
   });
 
   return data;
