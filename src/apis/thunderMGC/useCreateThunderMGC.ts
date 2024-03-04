3bb39fe4b6a5056b32616b0b679248342721decc
Jihee Shin <jiheenim27@gmail.com>
2024-03-04 10:20:35 +0900

    fix: url 변경

--- src/apis/thunderMGC/useCreateThunderMGC.ts
+++ src/apis/thunderMGC/useCreateThunderMGC.ts
@@ -3,7 +3,7 @@ import { CreateMGCReq } from '@/apis/mgc/queryFn';
 import { useMutation } from '@tanstack/react-query';
 
 export const createThunderMGC = async (createThunderMGCReq: CreateMGCReq) => {
-  return await client.put<CreateMGCReq>({ url: '/api/v1/mogakko/map', data: createThunderMGCReq });
+  return await client.put({ url: '/mogakko/map', data: createThunderMGCReq });
 };
 
 export const useCreateThunderMGC = () => {
