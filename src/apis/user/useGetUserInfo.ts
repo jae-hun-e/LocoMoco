d55240193149ef2125273582cbba5499c7f3a1bd
kimyou1102 <yoosk5485@naver.com>
2024-03-05 11:48:42 +0900

    feat:  유저 정보 api 연결

---
+++ src/apis/user/useGetUserInfo.ts
@@ -0,0 +1,29 @@
+import client from '@/apis/core';
+import { useQuery } from '@tanstack/react-query';
+
+export interface UserInfo {
+  userId: number;
+  nickname: string;
+  birth: string;
+  gender: 'FEMALE' | 'MALE';
+  temperature: number;
+  job: 'DEVELOPER' | 'JOB_SEEKER' | 'ETC';
+  email: string;
+  provider: 'KAKAO' | 'GITHUB';
+  profileImage: string | null;
+}
+
+const getUserInfo = async (userId: number) => {
+  return client.get<UserInfo>({
+    url: `/users/${userId}`,
+  });
+};
+
+const useGetUserInfo = (userId: number) => {
+  return useQuery({
+    queryKey: ['userInfo', userId] as const,
+    queryFn: () => getUserInfo(userId),
+  });
+};
+
+export default useGetUserInfo;
