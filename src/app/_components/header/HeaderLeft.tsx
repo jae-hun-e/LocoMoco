dad903c35b4af0f4fe6c64f199a2b91c4a3c7ff1
hun2__2 <wognskec@hanyang.ac.kr>
2024-03-05 11:58:29 +0900

    feat: 마이페이지 내의 세부페이지 헤더 추가

--- src/app/_components/header/HeaderLeft.tsx
+++ src/app/_components/header/HeaderLeft.tsx
@@ -5,11 +5,11 @@ import { ChevronLeftIcon } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const HeaderLeft = () => {
-  const pathname = usePathname().split('/')[1];
+  const pathname = usePathname().split('/').at(-1);
   const router = useRouter();
 
   // TODO: 에러 처리 중앙화 하기 [24/02/14]
-  if (!Object.keys(titleMap).includes(pathname)) throw new Error('잘못된 경로로 들어왔습니다.');
+  // if (!Object.keys(titleMap).includes(pathname)) throw new Error('잘못된 경로로 들어왔습니다.');
   const title = titleMap[pathname as keyof typeof titleMap];
 
   return (
