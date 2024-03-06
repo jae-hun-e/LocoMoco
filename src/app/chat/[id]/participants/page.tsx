44756d8e86a0a993fffc9f82a753e3ed59e97080
Jihee Shin <jiheenim27@gmail.com>
2024-03-06 10:43:20 +0900

    modify: 채팅 참여자 목록 공통 컴포넌트 사용하는 방식으로 변경

--- src/app/chat/[id]/participants/page.tsx
+++ src/app/chat/[id]/participants/page.tsx
@@ -1,5 +1,6 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+'use client';
+
+import UserList from '@/app/_components/UserInfoAndButton/UserList';
 
 const participants = [
   { username: 'Nickname1', profileImg: '/oh.png', userId: 1 },
@@ -9,31 +10,16 @@ const participants = [
 ];
 
 const Participants = () => {
-  return (
-    <section>
-      <ul className="flex flex-col gap-3">
-        {participants.map(({ username, profileImg }) => (
-          <li
-            key={username}
-            className="flex items-center justify-between border-b border-solid border-gray-300 pb-1"
-          >
-            <div className="flex items-center gap-2">
-              <Image
-                className="ml-1 rounded-3xl"
-                src={profileImg}
-                alt="good"
-                width={30}
-                height={30}
-                priority
-              />
-              <p>{username}</p>
-            </div>
+  const handleButtonClick = (targetId: number) => {
+    console.log(`${targetId}에게 후기를 보냅니다.`);
+  };
 
-            <Button className="mr-1 h-8 p-1">후기 보내기</Button>
-          </li>
-        ))}
-      </ul>
-    </section>
+  return (
+    <UserList
+      data={participants}
+      onClick={handleButtonClick}
+      buttonName="후기 보내기"
+    />
   );
 };
 export default Participants;
