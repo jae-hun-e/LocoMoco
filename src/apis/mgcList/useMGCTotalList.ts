16a9b222843405bbf3db61e6b9ed5d2cf2c8e26f
Jihee Shin <jiheenim27@gmail.com>
2024-03-08 17:00:42 +0900

    [#18] 검색 페이지 API 연결 (#56)

---
+++ src/apis/mgcList/useMGCTotalList.ts
@@ -0,0 +1,31 @@
+import { MGCList } from '@/types/MGCList';
+import { useQuery } from '@tanstack/react-query';
+import client from '../core';
+
+export interface TotalSearchProps {
+  search: string;
+  searchType: 'TOTAL' | 'LOCATION';
+  tags?: number[];
+}
+
+export const getMGCTotalList = async ({
+  search,
+  searchType = 'LOCATION',
+  tags,
+}: TotalSearchProps) => {
+  const { data } = await client.get<MGCList>({
+    url: `/mogakko/map`,
+    params: { tags, search, searchType },
+  });
+
+  return data;
+};
+
+const useMGCTotalList = ({ search, searchType, tags }: TotalSearchProps) => {
+  return useQuery({
+    queryKey: ['totalSearchMGC', search, searchType, tags] as const,
+    queryFn: () => getMGCTotalList({ search, searchType, tags }),
+  });
+};
+
+export default useMGCTotalList;
