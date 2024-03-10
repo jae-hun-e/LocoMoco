338914c23581b2f1277b84a3a336f01308339ba9
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-03-11 03:08:53 +0900

    refactor: remove Bearer

--- src/apis/core.ts
+++ src/apis/core.ts
@@ -10,7 +10,7 @@ const axiosInstance: AxiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
-    if (token) config.headers['Authorization'] = `Bearer ${token}`;
+    if (token) config.headers['Authorization'] = token;
 
     return config;
   },
