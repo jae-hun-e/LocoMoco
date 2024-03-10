52c8a6a3c26a4849e3e75e57b81c6ff54db817fd
Jihee Shin <jiheenim27@gmail.com>
2024-03-10 20:07:54 +0900

    feat: 신고 목록 item 구현

---
+++ src/app/mypage/report-list/_components/ReportListItem.tsx
@@ -0,0 +1,49 @@
+import { Button } from '@/components/ui/button';
+
+export interface Report {
+  reportId: number;
+  reporterId: number;
+  reportedId: number;
+  content: string;
+}
+
+interface ReportListItemProps {
+  data: Report;
+  onModifyBtnClick: (id: number) => void;
+  onDeleteBtnClick: (id: number) => void;
+}
+
+const ReportListItem = ({ data, onModifyBtnClick, onDeleteBtnClick }: ReportListItemProps) => {
+  return (
+    <li className="w-full flex-col border-b border-solid py-14pxr">
+      <div className="mb-10pxr flex items-center justify-between">
+        신고 대상: {data.reportedId}
+        <div className="flex gap-2">
+          <Button
+            type="button"
+            variant="outline"
+            className="border-1pxr h-30pxr w-50pxr border-solid border-main-1 text-xs text-main-1 hover:border-hover hover:bg-white hover:text-hover"
+            onClick={() => {
+              onModifyBtnClick(data.reportId);
+            }}
+          >
+            수정
+          </Button>
+          <Button
+            type="button"
+            variant="outline"
+            className="border-1pxr h-30pxr w-50pxr border-solid border-main-1 text-xs text-main-1 hover:border-hover hover:bg-white hover:text-hover"
+            onClick={() => {
+              onDeleteBtnClick(data.reportId);
+            }}
+          >
+            삭제
+          </Button>
+        </div>
+      </div>
+      <p>{data.content}</p>
+    </li>
+  );
+};
+
+export default ReportListItem;
