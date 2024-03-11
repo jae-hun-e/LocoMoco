0d46330e5445c9acd55a2107447ef1ce696871f7
Jihee Shin <jiheenim27@gmail.com>
2024-03-11 17:47:32 +0900

    feat: 블랙리스트 목록 받아오기 API 연결

--- src/app/mypage/black-list/page.tsx
+++ src/app/mypage/black-list/page.tsx
@@ -1,24 +1,32 @@
 'use client';
 
+// import useGetBlackList from '@/apis/blacklist/useGetBlacklist';
 import UserList from '@/app/_components/UserInfoAndButton/UserList';
 
 // TODO: layout.tsx 파일 생성 -> header 추가해야 함 [2024/03/06]
 
 const userList = [
-  { username: 'Nickname1', profileImg: '/oh.png', userId: 1 },
-  { username: 'Nickname2', profileImg: '/oh.png', userId: 2 },
-  { username: 'Nickname3', profileImg: '/oh.png', userId: 3 },
-  { username: 'Nickname4', profileImg: '/oh.png', userId: 4 },
+  {
+    userId: 1,
+    nickname: 'nickname1',
+    profileImage: {
+      imageId: 1,
+      path: '/oh.png',
+    },
+  },
 ];
 
 const BlackList = () => {
+  // const userId = 67;
+  // const { data } = useGetBlackList(userId);
+
   const handleButtonClick = (targetId: number) => {
     console.log(`유저 ${targetId}를 차단 해제합니다.`);
   };
 
   return (
     <UserList
-      data={userList}
+      data={userList ?? []}
       onClick={handleButtonClick}
       buttonName="차단 해제"
     />
