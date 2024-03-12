bfc2784ebeae0139d1b65f0b84cdb3107ba24499
Jihee Shin <jiheenim27@gmail.com>
2024-03-12 18:08:40 +0900

    feat: 블랙리스트 불러오기, 삭제 연결

--- src/app/mypage/black-list/page.tsx
+++ src/app/mypage/black-list/page.tsx
@@ -1,32 +1,26 @@
 'use client';
 
-// import useGetBlackList from '@/apis/blacklist/useGetBlacklist';
+import { useState } from 'react';
+import useDeleteBlacklist from '@/apis/blacklist/useDeleteBlacklist';
+import useGetBlackList from '@/apis/blacklist/useGetBlacklist';
 import UserList from '@/app/_components/UserInfoAndButton/UserList';
 
-// TODO: layout.tsx 파일 생성 -> header 추가해야 함 [2024/03/06]
-
-const userList = [
-  {
-    userId: 1,
-    nickname: 'nickname1',
-    profileImage: {
-      imageId: 1,
-      path: '/oh.png',
-    },
-  },
-];
-
 const BlackList = () => {
-  // const userId = 67;
-  // const { data } = useGetBlackList(userId);
+  // [Todo]: util에서 userId 가져오게 (2024/03/12)
+  const userId = 78;
+  const [targetId, setTargetId] = useState<number>(userId);
+  const { data } = useGetBlackList(userId);
+  const { deleteBlackList } = useDeleteBlacklist(userId, targetId);
 
   const handleButtonClick = (targetId: number) => {
     console.log(`유저 ${targetId}를 차단 해제합니다.`);
+    setTargetId(targetId);
+    deleteBlackList();
   };
 
   return (
     <UserList
-      data={userList ?? []}
+      data={data ?? []}
       onClick={handleButtonClick}
       buttonName="차단 해제"
     />
