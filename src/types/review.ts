12e519c46df37c56ed7d6b8a92b4ef6ccfee0ead
kimyou1102 <yoosk5485@naver.com>
2024-03-13 10:08:18 +0900

    modify: 타입 분리

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,3 +1,27 @@
+export interface ReviewSummary {
+  reviewId: number;
+  reviewContentId: number[];
+  content: string;
+  userId: number;
+  nickname: string;
+  job: string;
+  profileImage: {
+    imageId: number;
+    path: string;
+  } | null;
+  score: number;
+  createdAt: string;
+}
+
+export interface Reviews {
+  reviewId: number;
+  reviewerId: number;
+  revieweeId: number;
+  score: number;
+  reviewContentId: number[];
+  content: string;
+}
+
 export interface ReviewDetailType {
   reviewContentId: number[];
   content: string;
