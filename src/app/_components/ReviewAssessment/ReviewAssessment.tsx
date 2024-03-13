26ef299bae06615ddbce2ecb6a00a05a2ce28937
kimyou1102 <yoosk5485@naver.com>
2024-03-13 16:43:16 +0900

    modify: 받은 후기 평가 컴포넌트 공통 컴포넌트로 분리 후 사용

---
+++ src/app/_components/ReviewAssessment/ReviewAssessment.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import useGetReceivedReviews from '@/apis/review/useGetReceivedReviews';
+import useGetReviewContents from '@/apis/review/useGetReviewContents';
+import ReviewList from '../../_components/ReviewAssessment/ReviewList';
+
+const ReviewAssessment = ({ userId }: { userId: number }) => {
+  const { data: receivedReviews } = useGetReceivedReviews(userId);
+  const { data: reviewContents } = useGetReviewContents();
+
+  const goodReviews = receivedReviews ? receivedReviews.filter((review) => review.score > 2) : [];
+  const badReviews = receivedReviews ? receivedReviews.filter((review) => review.score <= 2) : [];
+
+  const goodList = reviewContents?.filter((content) => content.isPositive);
+  const badList = reviewContents?.filter((content) => !content.isPositive);
+
+  return (
+    <div className="grid gap-20pxr">
+      <ReviewList
+        list={goodList ?? []}
+        reviews={goodReviews}
+        title="🙂 받은 칭찬"
+      />
+      <ReviewList
+        list={badList ?? []}
+        reviews={badReviews}
+        title="😟 받은 비매너"
+      />
+    </div>
+  );
+};
+
+export default ReviewAssessment;
