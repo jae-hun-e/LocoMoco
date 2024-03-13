3ff17815c2dd471bfb934a55ed46954d17f8cb7c
hun2__2 <wognskec@hanyang.ac.kr>
2024-03-13 13:52:52 +0900

    feat: 참여시 모각코 데이터, 참여 여부 데이터 최신화

--- src/app/mgc/[id]/_hooks/useApplyMGC.ts
+++ src/app/mgc/[id]/_hooks/useApplyMGC.ts
@@ -1,17 +1,23 @@
 import client from '@/apis/core';
-import { useMutation } from '@tanstack/react-query';
+import { getMGCDetailQueryOption } from '@/apis/mgc/useGetMGCDetail';
+import { getCurrentApplyMGC } from '@/app/mgc/[id]/_hooks/useIsApply';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const participateMGC = async ({ MGCId, userId }: { MGCId: number; userId: number }) =>
   await client.post({ url: `/mogakko/map/${MGCId}/participate`, data: { userId } });
 
 export const useApplyMGC = () => {
+  const queryClient = useQueryClient();
   const { mutate, ...rest } = useMutation({
     mutationFn: participateMGC,
     onError: (error) => {
       console.log('mgc apply error', error);
     },
-    onSuccess: () => {
-      console.log('mgc apply success');
+    onSuccess: (_, variables) => {
+      const { MGCId, userId } = variables;
+
+      queryClient.invalidateQueries({ queryKey: getMGCDetailQueryOption(MGCId).queryKey });
+      queryClient.invalidateQueries({ queryKey: getCurrentApplyMGC({ MGCId, userId }).queryKey });
     },
   });
 
--- src/app/mgc/[id]/_hooks/useIsApply.ts
+++ src/app/mgc/[id]/_hooks/useIsApply.ts
@@ -1,19 +1,23 @@
 import client from '@/apis/core';
-import { useQuery } from '@tanstack/react-query';
+import { queryOptions, useQuery } from '@tanstack/react-query';
 
 interface CurrentApplyMGC {
-  isParticipated: true;
+  isParticipated: boolean;
 }
 const currentApplyMGC = async ({ MGCId, userId }: { MGCId: number; userId: number }) =>
   await client.get<CurrentApplyMGC>({
     url: `/mogakko/map/${MGCId}/participate`,
     params: { userId },
   });
-export const useIsApply = ({ MGCId, userId }: { MGCId: number; userId: number }) => {
-  const { data, ...rest } = useQuery({
+
+export const getCurrentApplyMGC = ({ MGCId, userId }: { MGCId: number; userId: number }) =>
+  queryOptions({
     queryKey: ['currentApplyMGC', MGCId, userId],
     queryFn: () => currentApplyMGC({ MGCId, userId }),
   });
+
+export const useIsApply = ({ MGCId, userId }: { MGCId: number; userId: number }) => {
+  const { data, ...rest } = useQuery(getCurrentApplyMGC({ MGCId, userId }));
 
   return { isParticipated: data?.isParticipated, ...rest };
 };
