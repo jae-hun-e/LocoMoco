27f8b8fa0fad2571760cbfe0767120745fb9f866
kimyou1102 <yoosk5485@naver.com>
2024-03-13 16:55:27 +0900

    modify: header에 표시될 경로 추가

--- src/constants/routeURL.ts
+++ src/constants/routeURL.ts
@@ -26,4 +26,6 @@ export const titleMap = {
   'end-join-mgc': '종료된 모각코',
   'like-mgc': '내가 찜한 모각코',
   'received-reviews-assessment': '받은 리뷰 평가',
+  another: '프로필',
+  'join-mgc': '참여한 모각코',
 } as const;
