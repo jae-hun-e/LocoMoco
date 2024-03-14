6fb2c5f7bd23c39e2c0a4b9579b0b4e2b390a162
kimyou1102 <yoosk5485@naver.com>
2024-03-14 12:34:43 +0900

    fix: 타입에 맞춰서 상수 변경

--- src/app/mypage/received-reviews-assessment/page.tsx
+++ src/app/mypage/received-reviews-assessment/page.tsx
@@ -4,7 +4,7 @@ import ReviewAssessment from '@/app/_components/ReviewAssessment/ReviewAssessmen
 
 const ReceivedReviews = () => {
   // TODO: 로컬스토리지 유틸함수가 머지되면 그걸로 로컬스토리지값 받아오게 수정하기 [24.03.06]
-  const userId = 77;
+  const userId = '77';
 
   return <ReviewAssessment userId={userId} />;
 };
