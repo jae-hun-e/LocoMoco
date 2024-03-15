718067170dfa05d3d52373084d2d48c68edf0f59
kimyou1102 <yoosk5485@naver.com>
2024-03-15 10:07:41 +0900

    modify: 더미데이터 제거

--- src/app/another/[userId]/join-mgc/page.tsx
+++ src/app/another/[userId]/join-mgc/page.tsx
@@ -6,41 +6,9 @@ import MGCList from '@/app/_components/MGCList/MGCList';
 const JoinPage = ({ params }: { params: { userId: string } }) => {
   const { data } = useGetJoinMGCByUserId(params.userId);
 
-  // TODO: suspense 적용 후 더미 제거 [24.03.15]
-  const dummy = [
-    {
-      id: 1,
-      title: '모각코111',
-      views: 12,
-      likeCount: 1,
-      maxParticipants: 3,
-      curParticipants: 1,
-      location: {
-        address: '구로동',
-        latitude: 37.456,
-        longitude: 126.6768,
-      },
-      tags: [240],
-    },
-    {
-      id: 2,
-      title: '모각코2222',
-      views: 12,
-      likeCount: 1,
-      maxParticipants: 3,
-      curParticipants: 1,
-      location: {
-        address: '구로동',
-        latitude: 37.456,
-        longitude: 126.6768,
-      },
-      tags: [238, 241],
-    },
-  ];
-
   return (
     <div>
-      <MGCList data={data ?? dummy} />
+      <MGCList data={data ?? []} />
     </div>
   );
 };
