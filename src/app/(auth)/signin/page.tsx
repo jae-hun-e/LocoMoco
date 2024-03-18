638e07412ebb593a6a51e780a58dc5946371f9be
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-03-18 15:10:02 +0900

    fix: redirection for production

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -16,7 +16,7 @@ const Signin = () => {
       router.replace('/');
     }
   }, [router]);
-
+  console.log(process.env.NODE_ENV);
   return (
     <section className="mx-auto flex h-svh flex-col items-center justify-center gap-3">
       <div className="mb-5 text-3xl font-bold">LocoMoco</div>
@@ -35,7 +35,7 @@ const Signin = () => {
       </Link>
 
       <Link
-        href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=http://localhost:3000/redirect/kakao&response_type=code`}
+        href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://locomoco.kro.kr'}/redirect/kakao&response_type=code`}
       >
         <button className="flex h-50pxr w-240pxr items-center justify-center gap-8 rounded-sm bg-[#fdd801] opacity-70 hover:opacity-100">
           <Image
