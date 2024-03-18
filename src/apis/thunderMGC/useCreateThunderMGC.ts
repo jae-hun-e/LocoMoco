4946882844acbab49b159c58004d7008c8efe87a
Jihee Shin <jiheenim27@gmail.com>
2024-03-18 19:57:27 +0900

    fix: 번개 모각코 API 오류 해결
    - put -> post로 변경
    - userId 값 추가
    - 지도에서 location 받아오기 추가

--- src/apis/thunderMGC/useCreateThunderMGC.ts
+++ src/apis/thunderMGC/useCreateThunderMGC.ts
@@ -1,14 +1,19 @@
 import client from '@/apis/core';
 import { CreateMGCReq } from '@/apis/mgc/queryFn';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const createThunderMGC = async (createThunderMGCReq: CreateMGCReq) => {
-  return await client.put({ url: '/mogakko/map', data: createThunderMGCReq });
+  return await client.post({ url: '/mogakko/map', data: createThunderMGCReq });
 };
 
 export const useCreateThunderMGC = () => {
+  const queryClient = useQueryClient();
+
   const { mutate, ...rest } = useMutation({
     mutationFn: createThunderMGC,
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ['totalSearchMGC'] });
+    },
   });
 
   return { createThunderMGC: mutate, ...rest };
