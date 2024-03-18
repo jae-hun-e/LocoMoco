8b6fae9fafe0704d7e40c40a134558977e7509da
hun2__2 <wognskec@hanyang.ac.kr>
2024-03-18 20:05:38 +0900

    feat: 헤더 + 외부이미지 URL 추가
    1. 헤더 css 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 import withPWAInit, { runtimeCaching } from '@ducanh2912/next-pwa';
+
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  images: {
+    domains: ["locomoco-image.s3.ap-northeast-2.amazonaws.com"], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+  },
 };
 
 const withPWA = withPWAInit({
