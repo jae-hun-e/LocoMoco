d9ac1619fbbe37f97c009c6bb33f0af4f805efeb
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-03-19 09:19:32 +0900

    modify: 로그인 하지 않았을 경우 아이콘 변경 + 툴팁

--- src/app/_components/CreateBtn.tsx
+++ src/app/_components/CreateBtn.tsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import ThunderModal from '@/app/(home)/_components/ThunderModal/ThunderModal';
 import { Button } from '@/components/ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { useThunderModalStore } from '@/store/thunderModalStore';
-import { Plus } from 'lucide-react';
+import { getItem } from '@/utils/storage';
+import { LockKeyhole, Plus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 const CreateBtn = () => {
   const router = useRouter();
   const [isCreateBtnOpen, setIsCreateBtnOpen] = useState(false);
   const { toggleModal } = useThunderModalStore();
+  const token = getItem<string | undefined>(localStorage, 'token');
 
   const handleCreateClick = (type: 'thunder' | 'normal') => {
     if (type === 'thunder') {
@@ -27,28 +30,43 @@ const CreateBtn = () => {
     >
       <PopoverTrigger asChild>
         <Button className="h-50pxr w-50pxr rounded-full bg-main-1 hover:bg-hover">
-          <Plus
-            className={`lucide lucide-plus ${isCreateBtnOpen ? 'rotate-45' : 'rotate-0'} transition`}
-          />
+          {token ? (
+            <Plus
+              className={`lucide lucide-plus ${isCreateBtnOpen ? 'rotate-45' : 'rotate-0'} transition`}
+            />
+          ) : (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <LockKeyhole onClick={() => router.push('/signin')} />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Login</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto border-0 bg-transparent p-10pxr text-sm shadow-none">
-        {/* TODO: 추후 디자인 논의 후 변경 [24/02/12] */}
-        <div className="flex flex-col gap-2 text-right">
-          <Button
-            className="cursor-pointer"
-            onClick={() => handleCreateClick('thunder')}
-          >
-            ⚡번개 모각코
-          </Button>
-          <Button
-            className="cursor-pointer"
-            onClick={() => handleCreateClick('normal')}
-          >
-            모각코
-          </Button>
-        </div>
-      </PopoverContent>
+      {token && (
+        <PopoverContent className="w-auto border-0 bg-transparent p-10pxr text-sm shadow-none">
+          {/* TODO: 추후 디자인 논의 후 변경 [24/02/12] */}
+          <div className="flex flex-col gap-2 text-right">
+            <Button
+              className="cursor-pointer"
+              onClick={() => handleCreateClick('thunder')}
+            >
+              ⚡번개 모각코
+            </Button>
+            <Button
+              className="cursor-pointer"
+              onClick={() => handleCreateClick('normal')}
+            >
+              모각코
+            </Button>
+          </div>
+        </PopoverContent>
+      )}
       <ThunderModal />
     </Popover>
   );
