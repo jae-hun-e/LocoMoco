c9d2f07fa9d1117ae17e82ebdcfe27a58ee917d5
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-03-19 09:10:10 +0900

    fix: 나에대한 후기 못쓰게 하기

--- src/app/_components/UserInfoAndButton/UserListItem.tsx
+++ src/app/_components/UserInfoAndButton/UserListItem.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import { getItem } from '@/utils/storage';
 
 export interface UserInfo {
   userId: number;
@@ -10,43 +11,38 @@ export interface UserInfo {
   } | null;
 }
 
-interface UserListProps {
+interface UserListItemProps {
   data: UserInfo;
   onClick: (targetId: number) => void;
   buttonName: string;
 }
 
-const UserListItem = ({ data, onClick, buttonName }: UserListProps) => {
+const UserListItem = ({ data, onClick, buttonName }: UserListItemProps) => {
+  const me = getItem(localStorage, 'userId');
+
   return (
     <li className="flex w-full items-center justify-between border-b border-solid py-14pxr">
       <div className="flex items-center gap-4">
         <Avatar className="ml-1 rounded-3xl">
-          {data.profileImage === null ? (
-            <AvatarImage
-              src={'/oh.png'}
-              alt="프로필 사진"
-            />
-          ) : (
-            <AvatarImage
-              src={data.profileImage.path}
-              alt="프로필 사진"
-            />
-          )}
+          <AvatarImage
+            src={data.profileImage ? data.profileImage.path : '/oh.png'}
+            alt="프로필 사진"
+          />
           <AvatarFallback>{data.nickname}</AvatarFallback>
         </Avatar>
         <p>{data.nickname}</p>
       </div>
 
-      <Button
-        type="button"
-        variant="outline"
-        className="border-1pxr w-84pxr border-solid border-main-1 text-sm text-main-1 hover:border-hover hover:bg-white hover:text-hover"
-        onClick={() => {
-          onClick(data.userId);
-        }}
-      >
-        {buttonName}
-      </Button>
+      {me !== data.userId.toString() && (
+        <Button
+          type="button"
+          variant="outline"
+          className="border-1pxr w-84pxr border-solid border-main-1 text-sm text-main-1 hover:border-hover hover:bg-white hover:text-hover"
+          onClick={() => onClick(data.userId)}
+        >
+          {buttonName}
+        </Button>
+      )}
     </li>
   );
 };
