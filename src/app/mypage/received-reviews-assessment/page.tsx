c7fb8d2d454995bc0661d4d5b555d973b7461b25
kimyou1102 <yoosk5485@naver.com>
2024-03-20 14:06:18 +0900

    fix: localStorage 클라이언트에서만 동작되게 수정

--- src/app/mypage/received-reviews-assessment/page.tsx
+++ src/app/mypage/received-reviews-assessment/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import ReviewAssessment from '@/app/_components/ReviewAssessment/ReviewAssessment';
 import { USER_ID_KEY, getItem } from '@/utils/storage';
 
 const ReceivedReviews = () => {
-  const userId = getItem<string>(localStorage, USER_ID_KEY);
+  const [userId, setUserId] = useState('');
 
-  return <ReviewAssessment userId={userId!} />;
+  useEffect(() => {
+    const userId = getItem<string>(localStorage, USER_ID_KEY);
+    if (userId) {
+      setUserId(userId);
+    }
+  }, []);
+  return <ReviewAssessment userId={userId} />;
 };
 
 export default ReceivedReviews;
