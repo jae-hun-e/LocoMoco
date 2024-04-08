a58f18927bc7fa9b1cd588c3947d872bf81757ae
kimyou1102 <yoosk5485@naver.com>
2024-04-08 18:04:48 +0900

    feat: 모각코 디테일 페이지 동적 메타데이터 생성

--- src/app/mgc/[id]/page.tsx
+++ src/app/mgc/[id]/page.tsx
@@ -1,5 +1,22 @@
 // import MGCDetailPage from '@/app/mgc/[id]/MGCPage';
+import { getMGCDetail } from '@/apis/mgc/useGetMGCDetail';
+import { Metadata } from 'next';
 import dynamic from 'next/dynamic';
+
+export async function generateMetadata({ params }: { params: { id: number } }): Promise<Metadata> {
+  const id = params.id;
+
+  const MGCDetail = await getMGCDetail(id);
+
+  return {
+    title: MGCDetail.MogakkoInfo.title,
+    description: MGCDetail.MogakkoInfo.content,
+    openGraph: {
+      title: MGCDetail.MogakkoInfo.title,
+      description: MGCDetail.MogakkoInfo.content,
+    },
+  };
+}
 
 const MGCDetailPage = dynamic(() => import('@/app/mgc/[id]/MGCPage'), { ssr: false });
 const MGCDetail = ({ params }: { params: { id: number } }) => {
