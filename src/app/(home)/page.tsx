83a6374315f87acf2c18a2659d56456f271bfb1d
kimyou1102 <yoosk5485@naver.com>
2024-04-21 17:06:06 +0900

    refactor: provider 패턴을 적용하여 Home 컴포넌트 구조 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,24 +1,81 @@
 'use client';
 
+import { useEffect, useRef, useState } from 'react';
 import useMGCTotalList from '@/apis/mgcList/useMGCTotalList';
+import Map from '@/app/_components/Map/Map';
 import useSearchInputValueStore from '@/store/useSearchValueStore';
-import MapSection from './_components/MapSection';
+import { MGCSummary } from '@/types/MGCList';
+import CreateBtn from '../_components/CreateBtn';
+import MGCList from '../_components/MGCList/MGCList';
+import Clusterer from '../_components/Map/Clusterer';
+import Geocoder from '../_components/Map/Geocoder';
+import Markers from '../_components/Map/Markers';
+import BottomSheet from './_components/BottomSheet';
 import SearchBarFilter from './_components/SearchBarFilter';
 
 const Home = () => {
+  const [MGCDataList, setMGCDataList] = useState<MGCSummary[]>([]);
+  const [open, setOpen] = useState(false);
+
   const { searchValue } = useSearchInputValueStore();
   const { data } = useMGCTotalList({
     search: searchValue.address,
-    searchType: 'LOCATION',
-    tags: searchValue.tags,
+    // TODO: 처음 위치의 주소를 받아오는 기능을 추가한 다음 LOCATION으로 변경하기 [24.04.20]
+    searchType: 'TOTAL',
   });
+
+  useEffect(() => {
+    if (data) {
+      setMGCDataList(data);
+    }
+  }, [data]);
+
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleMouseUp = () => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+  };
+
+  const openBottomSheetAndUpdate = (mapData: MGCSummary[]) => {
+    setMGCDataList(mapData);
+    setOpen(true);
+  };
+
+  const handleMarkerClick = (mapData: MGCSummary[]) => {
+    openBottomSheetAndUpdate(mapData);
+    handleMouseUp();
+  };
 
   return (
     <div className="relative -left-20pxr w-[100vw]">
-      <section className="flex w-full flex-col items-center">
-        <SearchBarFilter />
-      </section>
-      <MapSection data={data ?? []} />
+      <Map>
+        <Geocoder>
+          <section className="flex w-full flex-col items-center">
+            <SearchBarFilter />
+          </section>
+          <Clusterer>
+            <Markers
+              mapMGCData={data ?? []}
+              onMarkerClick={handleMarkerClick}
+              onClustererClick={openBottomSheetAndUpdate}
+            />
+          </Clusterer>
+        </Geocoder>
+      </Map>
+
+      <div className="absolute bottom-0 right-24pxr z-30">
+        <CreateBtn />
+      </div>
+      <div className="absolute bottom-15pxr z-10 flex w-full justify-center">
+        <BottomSheet
+          open={open}
+          setOpen={setOpen}
+        >
+          <MGCList data={MGCDataList} />
+        </BottomSheet>
+      </div>
     </div>
   );
 };
---
+++ src/app/_components/Map/Markers.tsx
@@ -0,0 +1,97 @@
+import { ReactNode, useCallback, useContext, useEffect } from 'react';
+import { MGCSummary } from '@/types/MGCList';
+import { clustererContext } from './Clusterer';
+import { MapContext } from './Map';
+
+interface MakerInfo {
+  marker: kakao.maps.Marker;
+  data: MGCSummary;
+}
+
+interface MarkersProps {
+  mapMGCData: MGCSummary[];
+  children?: ReactNode;
+  onMarkerClick: (mapData: MGCSummary[]) => void;
+  onClustererClick: (mapData: MGCSummary[]) => void;
+}
+
+const Markers = ({ mapMGCData, onMarkerClick, onClustererClick }: MarkersProps) => {
+  const map = useContext(MapContext);
+  const clusterer = useContext(clustererContext);
+
+  const setMarker = useCallback(() => {
+    const markersInfo: MakerInfo[] = [];
+    // TODO: 임시 아이콘 추후에 변경해야함 [24.04.21]
+    const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+    const imageSize = new kakao.maps.Size(24, 35);
+    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+    clusterer?.clear();
+
+    for (const mgc of mapMGCData) {
+      if (!(mgc.location.latitude && mgc.location.longitude)) continue;
+
+      const marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(mgc.location.latitude, mgc.location.longitude),
+        image: markerImage,
+      });
+
+      kakao.maps.event.addListener(marker, 'click', () => {
+        onMarkerClick([mgc]);
+      });
+
+      const newMarkerInfo = { data: mgc, marker: marker };
+
+      markersInfo.push(newMarkerInfo);
+    }
+
+    return markersInfo;
+  }, [clusterer, onMarkerClick, mapMGCData]);
+
+  const renderMarkerInCluseter = useCallback(() => {
+    const markersInfo = setMarker();
+    clusterer?.addMarkers(markersInfo.map((markerInfo) => markerInfo.marker));
+
+    kakao.maps.event.addListener(clusterer!, 'clusterclick', (cluster: kakao.maps.Cluster) => {
+      const clusterMarkers = cluster.getMarkers();
+      const markerList = [];
+
+      for (const clusterMarker of clusterMarkers) {
+        for (const markerInfo of markersInfo) {
+          if (markerInfo.marker === clusterMarker) {
+            markerList.push(markerInfo.data);
+          }
+        }
+      }
+
+      onClustererClick(markerList);
+    });
+  }, [clusterer, onClustererClick, setMarker]);
+
+  const renderMarkerInMap = useCallback(() => {
+    const markersInfo = setMarker();
+
+    for (const markerInfo of markersInfo) {
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: markerInfo.marker.getPosition(),
+      });
+
+      kakao.maps.event.addListener(marker, 'click', () => {
+        onMarkerClick([markerInfo.data]);
+      });
+    }
+  }, [map, onMarkerClick, setMarker]);
+
+  useEffect(() => {
+    if (clusterer) {
+      renderMarkerInCluseter();
+    } else if (map) {
+      renderMarkerInMap();
+    }
+  }, [clusterer, map, renderMarkerInCluseter, renderMarkerInMap]);
+
+  return null;
+};
+
+export default Markers;
