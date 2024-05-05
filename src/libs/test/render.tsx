cc321f89e4f12df5aff46cd5ad302b436fff4b51
kimyou1102 <yoosk5485@naver.com>
2024-05-05 17:35:32 +0900

    feat: QueryClientProvider로 컴포넌트 감싸서 렌더해주는 render컴포넌트 생성

---
+++ src/libs/test/render.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+export default function async(component: ReactNode) {
+  const user = userEvent.setup();
+
+  const queryClient = new QueryClient();
+
+  const renderedComponent = render(
+    <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>,
+  );
+
+  return {
+    user,
+    ...renderedComponent,
+  };
+}
