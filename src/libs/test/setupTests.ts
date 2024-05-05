ad50b19dd16855ba9ac759767bd2b7ca04a62d46
kimyou1102 <yoosk5485@naver.com>
2024-05-05 17:23:28 +0900

    chore: vitest 초기 세팅

---
+++ src/libs/test/setupTests.ts
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+afterAll(() => {
+  vi.resetAllMocks();
+});
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
