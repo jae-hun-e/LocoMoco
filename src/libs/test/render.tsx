02b9d522dd9de3ee118787022b94e4f5f20d74b1
kimyou1102 <yoosk5485@naver.com>
2024-05-14 20:07:14 +0900

    chore: 테스트용 QueryClient retry를 false로 설정
    - 테스트시 속도가 느려질 수 있어서 설정

--- src/libs/test/render.tsx
+++ src/libs/test/render.tsx
@@ -6,7 +6,13 @@ import userEvent from '@testing-library/user-event';
 export default function async(component: ReactNode) {
   const user = userEvent.setup();
 
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
 
   const renderedComponent = render(
     <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>,
