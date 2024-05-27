ab57f6b3a641308917cdd505059fbf3c0778be51
kimyou1102 <yoosk5485@naver.com>
2024-05-27 16:03:27 +0900

    modify: 불필요한 모킹 및 설정 제거

--- src/libs/test/setupTests.ts
+++ src/libs/test/setupTests.ts
@@ -16,17 +16,3 @@ afterAll(() => {
 });
 
 vi.mock('zustand');
-
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: vi.fn().mockImplementation((query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(),
-    removeListener: vi.fn(),
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
-  })),
-});
