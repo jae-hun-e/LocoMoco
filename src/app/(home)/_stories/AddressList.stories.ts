7a01c7bbd64f9c4f8a97481f1daaad234bc9cddd
kimyou1102 <yoosk5485@naver.com>
2024-05-29 19:55:08 +0900

    feat: AddressList 스토리 작성

---
+++ src/app/(home)/_stories/AddressList.stories.ts
@@ -0,0 +1,31 @@
+import { seochodongList, seokdongList } from '@/constants/searchResultAddressList';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import AddressList from '../_components/AddressList';
+
+const meta: Meta<typeof AddressList> = {
+  title: 'Components/AddressList',
+  component: AddressList,
+  args: { onClick: fn() },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const 서초동: Story = {
+  args: {
+    address: seochodongList,
+  },
+};
+
+export const 석동: Story = {
+  args: {
+    address: seokdongList,
+  },
+};
+
+export const EmptyList: Story = {
+  args: {
+    address: [],
+  },
+};
