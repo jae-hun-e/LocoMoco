3f1caeadb028ca3c4cefbd2e44057222fe2081f4
kimyou1102 <yoosk5485@naver.com>
2024-05-29 19:53:26 +0900

    modify: 주소리스트를 상수로 분리

---
+++ src/constants/searchResultAddressList.ts
@@ -0,0 +1,40 @@
+export const seochodongList = [
+  {
+    address_name: '서울 서초구 서초동',
+    latitude: 37.4900861966504,
+    longitude: 127.01953478052,
+  },
+  {
+    address_name: '서울 서초구 서초1동',
+    longitude: 127.019530273485,
+    latitude: 37.4901474659139,
+  },
+  {
+    address_name: '서울 서초구 서초2동',
+    longitude: 127.025076125816,
+    latitude: 37.4921394474613,
+  },
+  {
+    address_name: '서울 서초구 서초3동',
+    longitude: 127.01200750943,
+    latitude: 37.4837080639576,
+  },
+  {
+    address_name: '서울 서초구 서초4동',
+    longitude: 127.022170883244,
+    latitude: 37.5026854085131,
+  },
+];
+
+export const seokdongList = [
+  {
+    address_name: '경북 안동시 석동동',
+    latitude: 36.5748933942611,
+    longitude: 128.801404264416,
+  },
+  {
+    address_name: '경남 창원시 진해구 석동',
+    latitude: 35.1586340394401,
+    longitude: 128.700031729411,
+  },
+];
--- src/tests/SearchBarFilter.spec.tsx
+++ src/tests/SearchBarFilter.spec.tsx
@@ -1,37 +1,10 @@
 import { Address } from '@/apis/address/useAddressSearch';
 import AddressList from '@/app/(home)/_components/AddressList';
 import SearchBarFilter from '@/app/(home)/_components/SearchBarFilter';
+import { seochodongList } from '@/constants/searchResultAddressList';
 import render from '@/libs/test/render';
 import { screen } from '@testing-library/react';
 import { UserEvent } from '@testing-library/user-event';
-
-const address = [
-  {
-    address_name: '서울 서초구 서초동',
-    latitude: 37.4900861966504,
-    longitude: 127.01953478052,
-  },
-  {
-    address_name: '서울 서초구 서초1동',
-    longitude: 127.019530273485,
-    latitude: 37.4901474659139,
-  },
-  {
-    address_name: '서울 서초구 서초2동',
-    longitude: 127.025076125816,
-    latitude: 37.4921394474613,
-  },
-  {
-    address_name: '서울 서초구 서초3동',
-    longitude: 127.01200750943,
-    latitude: 37.4837080639576,
-  },
-  {
-    address_name: '서울 서초구 서초4동',
-    longitude: 127.022170883244,
-    latitude: 37.5026854085131,
-  },
-];
 
 let user: UserEvent;
 let textInput: HTMLElement;
@@ -50,7 +23,7 @@ describe('SearchBarFilter 컴포넌트 테스트', () => {
   it('"서초동" 검색 시 서초동에 해당하는 주소리스트가 나타난다.', async () => {
     await user.type(textInput, '서초동');
 
-    address.forEach((addressItem) => {
+    seochodongList.forEach((addressItem) => {
       const item = screen.getByText(addressItem.address_name);
       expect(item).toBeInTheDocument();
     });
@@ -76,12 +49,12 @@ describe('SearchBarFilter 컴포넌트 테스트', () => {
 
     await render(
       <AddressList
-        address={address}
+        address={seochodongList}
         onClick={mockHandleAddressClick}
       />,
     );
 
-    for (const addressItem of address) {
+    for (const addressItem of seochodongList) {
       const item = screen.getByText(addressItem.address_name);
       await user.click(item);
 
