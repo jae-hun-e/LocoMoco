fda1d130f256b1df80e43f3e5d20bea114496b8b
kimyou1102 <yoosk5485@naver.com>
2024-06-06 01:18:35 +0900

    modify: 불필요한 매개변수 제거를 통한 clickRatingStar 함수 단순화

--- src/tests/Review.spec.tsx
+++ src/tests/Review.spec.tsx
@@ -30,8 +30,7 @@ async function setup() {
   const completeButton = await screen.findByText('완료');
   const cancelButton = await screen.findByText('취소');
 
-  const clickRatingStar = async (ratingStars: HTMLElement[], index: number) =>
-    await user.click(ratingStars[index]);
+  const clickRatingStar = async (ratingStar: HTMLElement) => await user.click(ratingStar);
 
   const clickCompleteButton = async () => await user.click(completeButton);
 
@@ -101,17 +100,17 @@ describe('모각코가 끝난 후인 경우', () => {
       }
     };
 
-    await clickRatingStar(ratingStars, 0);
+    await clickRatingStar(ratingStars[0]);
     checkStarColor(await screen.findAllByLabelText('star'), 0);
 
-    await clickRatingStar(ratingStars, 1);
+    await clickRatingStar(ratingStars[1]);
     checkStarColor(await screen.findAllByLabelText('star'), 1);
   });
 
   it('별점 2점 이하 클릭 시 부정적인 후기를 남기는 컨텐츠가 뜬다.', async () => {
     const { clickRatingStar, ratingStars } = await setup();
 
-    await clickRatingStar(ratingStars, 1);
+    await clickRatingStar(ratingStars[1]);
 
     const badInfoText = screen.findByText('어떤 점이 별로였나요?');
     const textAreaInfoText = screen.findByText('아쉬웠던 점을 작성해주세요.');
@@ -130,7 +129,7 @@ describe('모각코가 끝난 후인 경우', () => {
   it('별점 3점이상 클릭 시 긍정적인 후기를 남기는 컨텐츠가 뜬다.', async () => {
     const { clickRatingStar, ratingStars } = await setup();
 
-    await clickRatingStar(ratingStars, 2);
+    await clickRatingStar(ratingStars[2]);
 
     const goodInfoText = screen.findByText('어떤 점이 좋았나요?');
     const textAreaInfoText = screen.findByText('좋았던 후기를 상대방에게 알려주세요!');
@@ -149,7 +148,7 @@ describe('모각코가 끝난 후인 경우', () => {
   it('아무것도 선택 하지 않고 완료 버튼을 누를 시 "하나 이상 선택해주세요." 라는 문구가 나타난다.', async () => {
     const { clickRatingStar, clickCompleteButton, ratingStars } = await setup();
 
-    await clickRatingStar(ratingStars, 0);
+    await clickRatingStar(ratingStars[0]);
     await clickCompleteButton();
 
     const waringText = screen.getByText('하나 이상 선택해주세요.');
@@ -161,7 +160,7 @@ describe('모각코가 끝난 후인 경우', () => {
   it('텍스트 입력창에 300자 이상 입력 시 "입력가능한 글자수는 300자입니다." 라는 문구가 나타난다.', async () => {
     const { user, clickRatingStar, completeButton, ratingStars } = await setup();
 
-    await clickRatingStar(ratingStars, 0);
+    await clickRatingStar(ratingStars[0]);
 
     const overSentence =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap";
@@ -179,12 +178,12 @@ describe('모각코가 끝난 후인 경우', () => {
     expect(waringText).toHaveStyle('color: rgb(239 68 68)');
   });
 
-  it('리뷰 체크박스들 중 하나 이상 선택 후 완료 버튼 클릭 시 "후기 작성이 완료되었습니다." 라는 toast 메시지가 뜬다.', async () => {
+  it('리뷰 체크박스들 중 하나 이상 선택 후 완료 버튼 클릭 시 "후기 작성이 완료되었습니다." 라는 toast 메시지가 나타난다.', async () => {
     const { user, clickRatingStar, ratingStars } = await setup();
 
     setItem(localStorage, USER_ID_KEY, 105);
 
-    await clickRatingStar(ratingStars, 2);
+    await clickRatingStar(ratingStars[2]);
 
     const completeButton = screen.getByText('완료');
     const checkItems = screen.getAllByRole('checkbox');
