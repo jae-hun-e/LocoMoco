1e8a20a0f7036d189ee3284631011725230a52ab
kimyou1102 <yoosk5485@naver.com>
2024-06-06 01:56:32 +0900

    modify: 일회성 요소는 변수로 만들지 않고, setup에 있는 요소는 setup 함수에서만 사용하도록 수정

--- src/tests/Review.spec.tsx
+++ src/tests/Review.spec.tsx
@@ -112,8 +112,8 @@ describe('모각코가 끝난 후인 경우', () => {
 
     await clickRatingStar(ratingStars[1]);
 
-    const badInfoText = screen.findByText('어떤 점이 별로였나요?');
-    const textAreaInfoText = screen.findByText('아쉬웠던 점을 작성해주세요.');
+    expect(await screen.findByText('어떤 점이 별로였나요?')).toBeInTheDocument();
+    expect(await screen.findByText('아쉬웠던 점을 작성해주세요.')).toBeInTheDocument();
 
     for (const { content, isPositive } of reviewContentList) {
       if (!isPositive) {
@@ -121,9 +121,6 @@ describe('모각코가 끝난 후인 경우', () => {
         expect(reviewContent).toBeInTheDocument();
       }
     }
-
-    expect(await badInfoText).toBeInTheDocument();
-    expect(await textAreaInfoText).toBeInTheDocument();
   });
 
   it('별점 3점이상 클릭 시 긍정적인 후기를 남기는 컨텐츠가 뜬다.', async () => {
@@ -131,8 +128,8 @@ describe('모각코가 끝난 후인 경우', () => {
 
     await clickRatingStar(ratingStars[2]);
 
-    const goodInfoText = screen.findByText('어떤 점이 좋았나요?');
-    const textAreaInfoText = screen.findByText('좋았던 후기를 상대방에게 알려주세요!');
+    expect(await screen.findByText('어떤 점이 좋았나요?')).toBeInTheDocument();
+    expect(await screen.findByText('좋았던 후기를 상대방에게 알려주세요!')).toBeInTheDocument();
 
     for (const { content, isPositive } of reviewContentList) {
       if (isPositive) {
@@ -140,9 +137,6 @@ describe('모각코가 끝난 후인 경우', () => {
         expect(reviewContent).toBeInTheDocument();
       }
     }
-
-    expect(await goodInfoText).toBeInTheDocument();
-    expect(await textAreaInfoText).toBeInTheDocument();
   });
 
   it('아무것도 선택 하지 않고 완료 버튼을 누를 시 "하나 이상 선택해주세요." 라는 문구가 나타난다.', async () => {
@@ -160,17 +154,12 @@ describe('모각코가 끝난 후인 경우', () => {
   it('텍스트 입력창에 300자 이상 입력 시 "입력가능한 글자수는 300자입니다." 라는 문구가 나타난다.', async () => {
     const { user, clickRatingStar, completeButton, ratingStars } = await setup();
 
-    await clickRatingStar(ratingStars[0]);
-
-    const overSentence =
+    const longText =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap";
-    const textarea = screen.getByPlaceholderText('여기에 적어주세요. (선택사항)');
 
-    await user.type(textarea, overSentence);
-
-    if (completeButton) {
-      await user.click(completeButton);
-    }
+    await clickRatingStar(ratingStars[0]);
+    await user.type(screen.getByPlaceholderText('여기에 적어주세요. (선택사항)'), longText);
+    await user.click(completeButton);
 
     const waringText = screen.getByText('입력가능한 글자수는 300자입니다.');
 
@@ -179,16 +168,12 @@ describe('모각코가 끝난 후인 경우', () => {
   });
 
   it('리뷰 체크박스들 중 하나 이상 선택 후 완료 버튼 클릭 시 "후기 작성이 완료되었습니다." 라는 toast 메시지가 나타난다.', async () => {
-    const { user, clickRatingStar, ratingStars } = await setup();
+    const { user, clickRatingStar, ratingStars, completeButton } = await setup();
 
     setItem(localStorage, USER_ID_KEY, 105);
 
     await clickRatingStar(ratingStars[2]);
-
-    const completeButton = screen.getByText('완료');
-    const checkItems = screen.getAllByRole('checkbox');
-
-    await user.click(checkItems[2]);
+    await user.click(screen.getAllByRole('checkbox')[2]);
     await user.click(completeButton);
 
     expect(toast).toBeCalledWith({
@@ -198,9 +183,7 @@ describe('모각코가 끝난 후인 경우', () => {
   });
 
   it('취소 버튼 클릭 시 모달이 꺼진다.', async () => {
-    const { user } = await setup();
-
-    const cancelButton = screen.getByText('취소');
+    const { user, cancelButton } = await setup();
 
     await user.click(cancelButton);
 
