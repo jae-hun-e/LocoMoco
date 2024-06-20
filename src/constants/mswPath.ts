6ff37f70ec7613d06ce7dd53cb0390fd4024fd92
kimyou1102 <yoosk5485@naver.com>
2024-06-21 01:26:34 +0900

    feat: mgcList를 가져오는 API 모킹을 위한 msw handler 추가

--- src/constants/mswPath.ts
+++ src/constants/mswPath.ts
@@ -2,5 +2,5 @@ export const api = {
   category: '/category?type=MOGAKKO',
   mgc: '/mogakko/map/228',
   address: 'https://dapi.kakao.com/v2/local/search/address.json',
-  mgcList: '/mogakko/map?searchType=LOCATION',
+  mgcList: '/mogakko/map',
 };
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,7 @@
 import { api } from '@/constants/mswPath';
 import { http } from 'msw';
 import response from './response';
+import mgcList from './response/mgcList.json';
 
 export const handlers = [
   ...[api.category, api.mgc].map((path) =>
@@ -12,6 +13,27 @@ export const handlers = [
       });
     }),
   ),
+
+  http.get(api.mgcList, ({ request }: { request: Request }) => {
+    const search = new URL(request.url).searchParams.get('search');
+    const tags = new URL(request.url).searchParams.getAll('tags').map(Number);
+
+    if (search || tags) {
+      const filtedMgcList = mgcList.data.filter((item) => {
+        const matchesSearch = search ? item.location.address.includes(search) : true;
+        const matchesTags = tags.length > 0 ? tags.every((tag) => item.tags.includes(tag)) : true;
+
+        return matchesSearch && matchesTags;
+      });
+
+      return new Response(JSON.stringify({ data: filtedMgcList }), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+  }),
+
   http.get(api.address, ({ request }: { request: Request }) => {
     const query = new URL(request.url).searchParams.get('query');
     if (query === '서초동') {
