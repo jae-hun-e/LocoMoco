21228976ff22efe034a17147c721a125fa0ddfa8
kimyou1102 <yoosk5485@naver.com>
2024-07-27 21:17:21 +0900

    feat: SearchBarFilter 스토리 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,9 +4,13 @@ import response from './response';
 import addressList from './response/addressList.json';
 import mgcList from './response/mgcList.json';
 
+const setBaseUrl = (path: string) => {
+  return `${process.env.NEXT_PUBLIC_BASE_URL}${path}`;
+};
+
 export const handlers = [
-  ...[api.category, api.mgc, api.users, api.reviewContents].map((path) =>
-    http.get(`${path}`, () => {
+  ...[api.mgc, api.users, api.reviewContents].map((path) =>
+    http.get(setBaseUrl(path), () => {
       return new Response(JSON.stringify(response[path]), {
         headers: {
           'Content-Type': 'application/json',
@@ -15,14 +19,25 @@ export const handlers = [
     }),
   ),
 
-  http.get(api.mgcList, ({ request }: { request: Request }) => {
+  http.get(setBaseUrl(api.category), ({ request }: { request: Request }) => {
+    const type = new URL(request.url).searchParams.get('type');
+    if (type === 'MOGAKKO') {
+      return new Response(JSON.stringify(response[api.category]), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+  }),
+  http.get(setBaseUrl(api.mgcList), ({ request }: { request: Request }) => {
     const search = new URL(request.url).searchParams.get('search');
     const tags = new URL(request.url).searchParams.getAll('tags').map(Number);
 
     if (search || tags) {
       const filtedMgcList = mgcList.data.filter((item) => {
         const matchesSearch = search ? item.location.address.includes(search) : true;
-        const matchesTags = tags.length > 0 ? tags.every((tag) => item.tags.includes(tag)) : true;
+        const matchesTags = tags.length > 0 ? tags.some((tag) => item.tags.includes(tag)) : true;
+        // const matchesTags = tags.length > 0 ? tags.every((tag) => item.tags.includes(tag)) : true;
 
         return matchesSearch && matchesTags;
       });
@@ -35,7 +50,7 @@ export const handlers = [
     }
   }),
 
-  http.get(api.address, ({ request }: { request: Request }) => {
+  http.get(setBaseUrl(api.address), ({ request }: { request: Request }) => {
     const query = new URL(request.url).searchParams.get('query');
 
     if (query) {
@@ -50,7 +65,7 @@ export const handlers = [
       });
     }
   }),
-  http.post(api.createReview, async () => {
+  http.post(setBaseUrl(api.createReview), async () => {
     return new Response(JSON.stringify(response[api.createReview]), {
       headers: {
         'Content-Type': 'application/json',
