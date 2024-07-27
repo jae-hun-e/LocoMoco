242d33f528ca47ea36f2922f8b204273b40134f7
kimyou1102 <yoosk5485@naver.com>
2024-07-27 21:12:16 +0900

    chore: storybook msw 설정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,9 @@
 import type { Preview } from '@storybook/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+import { handlers } from '../src/mocks/handlers';
 import '../src/styles/globals.css';
+
+initialize();
 
 const preview: Preview = {
   parameters: {
@@ -9,7 +13,11 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    msw: {
+      handlers,
+    },
   },
+  loaders: [mswLoader],
 };
 
 export default preview;
