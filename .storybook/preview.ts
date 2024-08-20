c2fd42c1d478170beca457a12435540a93e4c7b7
kimyou1102 <yoosk5485@naver.com>
2024-08-20 17:25:09 +0900

    chore: 크로매틱 배포를 위한 MSW 서비스 워커 경로 추가
    - 크로매틱 배포 환경에서 MSW가 올바르게 동작하도록 '/mockServiceWorker.js' 경로를 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,17 @@
 import type { Preview } from '@storybook/react';
 import { initialize, mswLoader } from 'msw-storybook-addon';
+import { setupWorker } from 'msw/browser';
 import { handlers } from '../src/mocks/handlers';
 import '../src/styles/globals.css';
 
 initialize();
+
+const worker = setupWorker(...handlers(true));
+worker.start({
+  serviceWorker: {
+    url: './mockServiceWorker.js',
+  },
+});
 
 const preview: Preview = {
   parameters: {
