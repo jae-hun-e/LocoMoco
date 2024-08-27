425c3073628236d10adaf63eb9e9af83cdb38565
kimyou1102 <yoosk5485@naver.com>
2024-08-27 21:40:13 +0900

    fix: 스토리북 msw 오류 해결
    - 카카오 주소 API가 전체 URL이어서 "도메인/전체 URL" 형식으로 잘못된 요청이 발생하는 문제 수정
    - 도메인 포함된 URL과 전체 URL을 분리하여 처리

--- src/mocks/response/index.ts
+++ src/mocks/response/index.ts
@@ -1,4 +1,4 @@
-import { api } from '../../constants/mswPath';
+import { apiFullUrl, apiPath } from '../../constants/mswPath';
 import addressList from './addressList.json';
 import categories from './categories.json';
 import createReview from './createReview.json';
@@ -7,13 +7,13 @@ import reviewContentList from './reviewContentList.json';
 import user from './user.json';
 
 const path = {
-  [api.category]: categories,
-  [api.mgc]: categories,
-  [api.address]: addressList,
-  [api.mgcList]: mgcList,
-  [api.users]: user,
-  [api.reviewContents]: reviewContentList,
-  [api.createReview]: createReview,
+  [apiPath.category]: categories,
+  [apiPath.mgc]: categories,
+  [apiFullUrl.address]: addressList,
+  [apiPath.mgcList]: mgcList,
+  [apiPath.users]: user,
+  [apiPath.reviewContents]: reviewContentList,
+  [apiPath.createReview]: createReview,
 };
 
 export default path;
