45468339df78315d9e8989f77436e3cb9d874096
kimyou1102 <yoosk5485@naver.com>
2024-09-13 01:29:36 +0900

    feat: SearchBar 공통 컴포넌트 생성
    - type props에 따라 서치바의 스타일이 달라지게 구현

---
+++ src/app/_components/SearchBar.tsx
@@ -0,0 +1,51 @@
+import { MutableRefObject } from 'react';
+import { cn } from '@/libs/utils';
+import { Search } from 'lucide-react';
+
+interface SearchBarProps {
+  onInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  onFocus?: () => void;
+  onBlur?: () => void;
+  inputRef: MutableRefObject<HTMLInputElement | null>;
+  type: 'radius' | 'flat';
+  className?: string;
+}
+
+const SearchBar = ({
+  onInputChange,
+  placeholder,
+  onFocus,
+  onBlur,
+  inputRef,
+  type,
+  className,
+}: SearchBarProps) => {
+  return (
+    <div
+      id="input-wrap"
+      className={cn(
+        'flex h-50pxr flex-row items-center bg-layer-1 pr-10pxr',
+        type === 'flat' ? 'rounded-none border-b' : 'rounded-[30px] border',
+        className,
+      )}
+    >
+      <Search
+        width={20}
+        height={20}
+        color="gray"
+        className="m-10pxr"
+      />
+      <input
+        ref={inputRef}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onChange={onInputChange}
+        placeholder={placeholder}
+        className="mr-10pxr h-10 w-full text-sm focus:outline-none"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
