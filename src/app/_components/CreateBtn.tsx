4d67d08e060203f5857a39710443eb358cfcc6c0
kimyou1102 <yoosk5485@naver.com>
2024-10-14 15:57:29 +0900

    fix: 바텀시트 오픈 시 생성 버튼 클릭 후 닫히는 문제 해결
    - onOpenChange에서 open 상태를 직접 변경하지 않고 state로 분리하여 제어
    - 트리거 시 state를 true로 설정하고, 다른 영역 클릭 시 false로 변경하여 문제 해결

--- src/app/_components/CreateBtn.tsx
+++ src/app/_components/CreateBtn.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import useA2HS from '@/hooks/useA2HS';
+import useClickAway from '@/hooks/useClickaway';
 import { useThunderModalStore } from '@/store/thunderModalStore';
 import { getItem } from '@/utils/storage';
 import { LockKeyhole, Plus } from 'lucide-react';
@@ -17,6 +18,8 @@ const CreateBtn = () => {
   const [isCreateBtnOpen, setIsCreateBtnOpen] = useState(false);
   const { toggleModal } = useThunderModalStore();
   const { installApp } = useA2HS();
+  const clickAwayRef = useClickAway<HTMLDivElement>(() => setIsCreateBtnOpen(false));
+
   const handleCreateClick = (type: 'thunder' | 'normal') => {
     if (type === 'thunder') {
       toggleModal();
@@ -26,68 +29,67 @@ const CreateBtn = () => {
   };
 
   const handleButtonClick = () => {
+    setIsCreateBtnOpen(true);
     if (token === undefined) {
       router.push('/signin');
     }
   };
 
   return (
-    <Popover
-      onOpenChange={(open) => {
-        setIsCreateBtnOpen(open);
-      }}
-    >
-      <PopoverTrigger asChild>
-        <Button
-          onClick={handleButtonClick}
-          role="button"
-          className="h-50pxr w-50pxr rounded-full bg-main-1 hover:bg-hover"
-        >
-          {token ? (
-            <Plus
-              className={`lucide lucide-plus ${isCreateBtnOpen ? 'rotate-45' : 'rotate-0'} transition`}
-            />
-          ) : (
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <LockKeyhole />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Login</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          )}
-        </Button>
-      </PopoverTrigger>
-      {token && (
-        <PopoverContent className="w-auto border-0 bg-transparent p-10pxr text-sm shadow-none">
-          {/* TODO: 추후 디자인 논의 후 변경 [24/02/12] */}
-          <div className="flex flex-col gap-2 text-right">
-            <Button
-              className="cursor-pointer"
-              onClick={() => handleCreateClick('thunder')}
-            >
-              ⚡번개 모각코
-            </Button>
-            <Button
-              className="cursor-pointer"
-              onClick={() => handleCreateClick('normal')}
-            >
-              모각코
-            </Button>
-            <Button
-              className="cursor-pointer"
-              onClick={installApp}
-            >
-              앱 설치
-            </Button>
-            <A2HS />
-          </div>
-        </PopoverContent>
-      )}
-    </Popover>
+    <div ref={clickAwayRef}>
+      <Popover open={isCreateBtnOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            onClick={handleButtonClick}
+            role="button"
+            className="h-50pxr w-50pxr rounded-full bg-main-1 hover:bg-hover"
+          >
+            {token ? (
+              <Plus
+                className={`lucide lucide-plus ${isCreateBtnOpen ? 'rotate-45' : 'rotate-0'} transition`}
+              />
+            ) : (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <LockKeyhole />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Login</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+          </Button>
+        </PopoverTrigger>
+        {token && (
+          <PopoverContent className="w-auto border-0 bg-transparent p-10pxr text-sm shadow-none">
+            {/* TODO: 추후 디자인 논의 후 변경 [24/02/12] */}
+            <div className="flex flex-col gap-2 text-right">
+              <Button
+                className="cursor-pointer"
+                onClick={() => handleCreateClick('thunder')}
+              >
+                ⚡번개 모각코
+              </Button>
+              <Button
+                className="cursor-pointer"
+                onClick={() => handleCreateClick('normal')}
+              >
+                모각코
+              </Button>
+              <Button
+                className="cursor-pointer"
+                onClick={installApp}
+              >
+                앱 설치
+              </Button>
+              <A2HS />
+            </div>
+          </PopoverContent>
+        )}
+      </Popover>
+    </div>
   );
 };
 
